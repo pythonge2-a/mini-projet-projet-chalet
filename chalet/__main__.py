d6175e80d91e0bbe2e309dd2340f32b600c7263a
Thomas Bachmann <thomas.bachmann@heig-vd.ch>
2024-12-06 10:01:23 +0100

    Created Web Server

--- chalet/__main__.py
+++ chalet/__main__.py
@@ -2,8 +2,7 @@ from sim import __sim__ as sm
 from meteo import __meteo__ as mt
 from database import __database__ as db
 def main():
-    print("Chalet is running")
-    
+    print('hello')
 
 if __name__ == '__main__':
     main()
