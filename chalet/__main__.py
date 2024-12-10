fafe3ed889db78d8a7e2ea2bc4a33695e42740f9
Aurore Delessert <aurore.delessert@heig-vd.ch>
2024-12-10 09:59:25 +0100

    tests main

--- chalet/__main__.py
+++ chalet/__main__.py
@@ -1,8 +1,12 @@
-from reading import __reading__
+from reading import __reading__ as rd
 
 
 def main():
     print("Chalet is running")
+    temp = rd.get_current_temperature()
+    weather = rd.current_weather_condition()
+    print("Weather: ", weather)
+    
 
 if __name__ == '__main__':
     main()
