65493cdf85dff7e2cc747a9a5b30683a4a186920
Thomas Bachmann <thomas.bachmann@heig-vd.ch>
2024-12-10 08:45:09 +0100

    Two pages work with a commun base

--- chalet/pages/urls.py
+++ chalet/pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, graph_view
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('graphics/', graph_view, name='graphics'),
 ]
--- chalet/pages/views.py
+++ chalet/pages/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request):
     return render(request, 'home.html')
+
+def graph_view(request):
+    return render(request, 'graphics.html')
