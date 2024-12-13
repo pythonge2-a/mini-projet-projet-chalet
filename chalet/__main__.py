851b80723c599b6bf6d3f311eb25e92425b8391b
Aurore Delessert <aurore.delessert@heig-vd.ch>
2024-12-13 09:44:29 +0100

    add function in main

--- chalet/__main__.py
+++ chalet/__main__.py
@@ -3,8 +3,10 @@ from reading import __reading__ as rd
 def main():
     print("Chalet is running")
     temp = rd.get_current_temperature()
+    weather = rd.get_current_weather_condition()
     rd.show_all_graphs()
     print("Current temperature is: ", temp)
+    print("Current weather is: ", weather)
     
 
 if __name__ == '__main__':
