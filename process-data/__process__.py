8843ad5890989026ef315898445034ccb808d720
Aurore Delessert <aurore.delessert@heig-vd.ch>
2025-01-07 09:31:18 +0100

    changement

--- process-data/__process__.py
+++ process-data/__process__.py
@@ -19,19 +19,41 @@ def get_current_luminosity():
 def get_state_lamp():
     return rd.get_current_lamp_state()
 
+#
+# Automation, logique
+#
+
 def light_on():
     for i in get_state_lamp():
-        if (get_current_luminosity() < 100):
+        if (get_current_luminosity() < 100):    # 100 lux
             return True
         else:
             return False
         
-def temperature_on():
-    if get_current_temperature() > 25:
-        return False
-    else:
+def heating_on_living_room():
+    if get_current_temperature() < 21:  # 21°C
         return True
+    else:
+        return False
+    
+def heating_on_bedroom():
+    if get_current_temperature() < 18:  # 18°C
+        return True
+    else:
+        return False
+    
+def velux_open():
+    if get_current_temperature() > 20 and get_current_luminosity() > 100:   # 20°C et 100 lux
+        return True
+    else:
+        return False
+    
+def store_open():
+    if get_current_temperature() > 20 and get_current_luminosity() > 100:   # 20°C et 100 lux
+        return True
+    else:
+        return False
 
-        
+
 
 
