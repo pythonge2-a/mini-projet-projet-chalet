2c7dfb97c01acfe32baf0e7c47b3f4bd86f4115f
Aurore Delessert <aurore.delessert@heig-vd.ch>
2025-01-07 08:47:30 +0100

    add temp_on()

--- process-data/__process__.py
+++ process-data/__process__.py
@@ -16,8 +16,22 @@ def get_current_temperature():
 def get_current_luminosity():
     return rd.get_current_luminosity()
 
+def get_state_lamp():
+    return rd.get_current_lamp_state()
+
 def light_on():
-    if (get_current_luminosity() < 100):
-        return True
-    else:
+    for i in get_state_lamp():
+        if (get_current_luminosity() < 100):
+            return True
+        else:
+            return False
+        
+def temperature_on():
+    if get_current_temperature() > 25:
         return False
+    else:
+        return True
+
+        
+
+
