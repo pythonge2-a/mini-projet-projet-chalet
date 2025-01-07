8ed703ca84659533174c0bf1b80d43bfc3cdac0c
Thomas Bachmann <thomas.bachmann@heig-vd.ch>
2025-01-07 10:29:24 +0100

    Update captors page

--- chalet/__main__.py
+++ chalet/__main__.py
@@ -1,8 +1,16 @@
 from sim import __sim__ as sm
 from meteo import __meteo__ as mt
 from database import __database__ as db
-def main():
-    print('hello')
+import os
+import sys
+from pathlib import Path
 
-if __name__ == '__main__':
+sys.path.append(str(Path(__file__).resolve().parent))
+
+from chalet.manage import main as manage_main
+
+def main():
+    manage_main('runserver', '0.0.0.0:8000')
+
+if __name__ == "__main__":
     main()
