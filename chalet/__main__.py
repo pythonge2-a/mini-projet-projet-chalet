0117507f656aa2c2a4c1b0011d32b95fd6726e19
Thomas Bachmann <thomas.bachmann@heig-vd.ch>
2025-01-07 20:56:22 +0100

    add staticfiles

--- chalet/__main__.py
+++ chalet/__main__.py
@@ -10,7 +10,14 @@ sys.path.append(str(Path(__file__).resolve().parent))
 from chalet.manage import main as manage_main
 
 def main():
-    manage_main('runserver', '0.0.0.0:8000')
+    if len(sys.argv) > 1:
+        command = sys.argv[1]
+        args = sys.argv[2:]
+    else:
+        command = 'runserver'
+        args = ['0.0.0.0:8000']
+    
+    manage_main(command, *args)
 
 if __name__ == "__main__":
     main()
