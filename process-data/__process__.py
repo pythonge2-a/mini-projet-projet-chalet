323535b599b0995192fb4a679d875afd237b207a
Aurore Delessert <aurore.delessert@heig-vd.ch>
2025-01-17 09:15:35 +0100

    add README.md + pytest + changes in process

--- process-data/__process__.py
+++ process-data/__process__.py
@@ -4,56 +4,65 @@
 
 import database as db
 
-#
-# Récupère les données actuelles avec reading
-#
-
-def get_current_date_time():
-    return db.get_current_date_time()
-
-def get_current_temperature():
-    return db.get_current_temperature()
-
-def get_current_luminosity():
-    return db.get_current_luminosity()
-
-def get_state_lamp():
-    return db.get_current_lamp_state()
-
-#
-# Automation, logique
-#
-
+def light_on_living_room():
+    if db.get_device_state('luminosity_living_room') < 100:    # 100 lux
+        db.update_device_state('lamp_living_room', 'on')
+    else:
+        db.update_device_state('lamp_living_room', 'off')
+        
 def light_on_bedroom():
-    for i in get_state_lamp():
-        if (get_current_luminosity() < 100):    # 100 lux
-            db.update_device_state('lamp_bedroom', 'on')
-        else:
-            db.update_device_state('lamp_bedroom', 'off')
+    if db.get_device_state('luminosity_bedroom') < 100:    # 100 lux
+        db.update_device_state('lamp_bedroom', 'on')
+    else:
+        db.update_device_state('lamp_bedroom', 'off')
+        
+def light_on_kitchen():
+    if db.get_device_state('luminosity_kitchen') < 100:    # 100 lux
+        db.update_device_state('lamp_kitchen', 'on')
+    else:
+        db.update_device_state('lamp_kitchen', 'off')
+        
+def light_on_bathroom():
+    if db.get_device_state('luminosity_bathroom') < 100:    # 100 lux
+        db.update_device_state('lamp_bathroom', 'on')
+    else:
+        db.update_device_state('lamp_bathroom', 'off')
         
 def heating_on_living_room():
-    if get_current_temperature() < 21:  # 21°C
+    if db.get_device_state('temperature sensor_rest_of_chalet') < 21:  # 21°C
         db.update_device_state('heater_rest_of_chalet', 'on')
     else:
         db.update_device_state('heater_rest_of_chalet', 'off')
     
 def heating_on_bedroom():
-    if get_current_temperature() < 18:  # 18°C
+    if db.get_device_state('temperature sensor_bedroom') < 18:  # 18°C
         db.update_device_state('heater_bedroom', 'on')
     else:
         db.update_device_state('heater_bedroom', 'off')
     
 def velux_open():
-    if get_current_temperature() > 20 and get_current_luminosity() > 100:   # 20°C et 100 lux
-        db.update_device_state('velux_living_room', 'open')
+    if db.get_device_state('humidity sensor_all') > 20 and db.get_device_state('luminosity_living_room') > 100:   # 20°C et 100 lux
+        db.update_device_state('skylight_living_room', 'open')
     else:
-        db.update_device_state('velux_living_room', 'closed')
+        db.update_device_state('skylight_living_room', 'closed')
     
 def store_open():
-    if get_current_temperature() > 20 and get_current_luminosity() > 100:   # 20°C et 100 lux
+    if db.get_device_state('temperature sensor_rest_of_chalet') > 20 and db.get_device_state('luminosity_living_room') > 100:   # 20°C et 100 lux
         db.update_device_state('store_living_room', 'open')
     else:
+        db.update_device_state('store_living_room', 'closed')
         
+def update_data():
+    light_on_living_room()
+    light_on_bedroom()
+    light_on_kitchen()
+    light_on_bathroom()
+    heating_on_living_room()
+    heating_on_bedroom()
+    velux_open()
+    store_open()
+    
+    print("Data updated successfully")
 
 
 
