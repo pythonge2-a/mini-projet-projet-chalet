bc11c507098aaafedfda366b66924a0a735b34ad
Thomas Bachmann <thomas.bachmann@heig-vd.ch>
2025-01-20 21:04:58 +0100

    Modification to command

--- chalet/__main__.py
+++ chalet/__main__.py
@@ -1,5 +1,6 @@
 from meteo import __meteo__ as mt
 from database import __database__ as db
+from chalet.mqtt_client import print_connected_clients, publish_message
 import os
 import sys
 from pathlib import Path
@@ -11,6 +12,19 @@ from chalet.manage import main as manage_main
 def main():
     if len(sys.argv) > 1:
         command = sys.argv[1]
+
+        if command == "list_devices":
+            print_connected_clients()
+            return
+        elif command == "publish_mqtt":
+            if len(sys.argv) > 2:
+                message = sys.argv[2]
+                publish_message(message)
+                return
+            else:
+                print("Error: No message provided")
+                return
+            
         args = sys.argv[2:]
     else:
         command = 'runserver'
